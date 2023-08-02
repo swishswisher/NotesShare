fe61af835173fea711ee5a8d3f537cf70f25c9a7
Swishswisher <89091617+Swishswisher@users.noreply.github.com>
2023-08-02 12:48:41 +0300

    first commit

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,54 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB()
+
+        const prompt = await Prompt.findById(params.id).populate("creator")
+        if (!prompt) return new Response("Note Not Found", { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+
+    } catch (error) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
+}
+
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+
+        // Find the existing prompt by ID
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if (!existingPrompt) {
+            return new Response("Note not found", { status: 404 });
+        }
+
+        // Update the prompt with new data
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response("Successfully updated the Notes", { status: 200 });
+    } catch (error) {
+        return new Response("Error Updating Note", { status: 500 });
+    }
+};
+
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        // Find the prompt by ID and remove it
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Note deleted successfully", { status: 200 });
+    } catch (error) {
+        return new Response("Error deleting note", { status: 500 });
+    }
+};
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from "@components/Feed";
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      Notes Share is a tool for discovering, creating and sharing creative notes
+    </p>
+
+    <Feed />
+  </section>
+);
+
+export default Home;
